4a860d93bb80588b129728e1cb73342440214345
smckinney0706 <smckinney0706@gmail.com>
2017-02-20 13:37:04 -0600

    added two sets of boxes to scroll in from the right and left using css and JQuery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@
 // });
 
 $(document).scroll(function(){
-	console.log( $(document).scrollTop() );
+	// console.log( $(document).scrollTop() );
 
 	$("#frontclouds").css("left", String($(document).scrollTop() - 250) + "px" );
 	$("#middleclouds").css("left", String($(document).scrollTop() * .5) + "px" );
@@ -15,11 +15,24 @@ $(document).scroll(function(){
 });
 
 $(document).mousemove(function(p){
-	console.log(p.pageX + "," + p.pageY);
+	// console.log(p.pageX + "," + p.pageY);
 	if(p.pageX > 500 && p.pageX < 700){
 		$("#frontclouds").css("left", String(p.pageX - 700) + "px" );
 		$("#middleclouds").css("left", String(p.pageX * .5 - 300) + "px" );
 		$("#backclouds").css("left", String(p.pageX * .2 - 100) + "px" );
 	}
 	
+});
+
+
+
+$(document).scroll(function(){
+	 console.log( $(document).scrollTop() );
+	 
+	 $("#row1").css("right", String($(document).scrollTop() - 1000) + "px");
+
+	 if ( $(document).scrollTop() > 0 ) {
+	 	$("#row2").css("left", String($(document).scrollTop() - 1000) + "px");
+	 }
+	 
 });
